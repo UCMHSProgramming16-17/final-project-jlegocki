46df615cd394e3cd6de0fe958396073a57928e72
Jakub Legocki <jlegocki@ucvts.org>
2017-01-13 15:40:52 +0000

    Starting out final project file.

---
+++ jlegocki-final_project.py
@@ -0,0 +1,13 @@
+#import modules for later use
+import numpy as np
+import pandas as pd
+from bokeh.plotting import figure, output_file, save
+from bokeh.charts import Line, output_file, save 
+
+
+#use college scorecard csv data file to get info on sat scores during the 2014-2015 school year 
+
+#download csv file for data
+
+#read from file to get info on scores 
+
